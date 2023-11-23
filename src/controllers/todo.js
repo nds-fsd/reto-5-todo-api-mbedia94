75ac9d52eefad97630affd179d67b8fccb30b091
mbedia94 <mbedia94@gmail.com>
2023-11-23 01:45:20 +0100

    chore(backend-todo):añadir try catch

--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -1,62 +1,82 @@
 const { todoList } = require("../data/todoList");
 
 const getAllTodo = (req, res) => {
-  res.status(200).json({ Task: todoList });
+  try {
+    res.status(200).json({ Task: todoList });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const getTodoById = (req, res) => {
   const todoId = parseInt(req.params.id);
   const todoFound = todoList.find((todo) => todo.id === todoId);
-  if (todoFound) {
-    res.status(200).json(todoFound);
-  } else {
-    res.status(404).json({ error: "Todo not found" });
+  try {
+    if (todoFound) {
+      res.status(200).json(todoFound);
+    } else {
+      res.status(404).json({ error: "Todo not found" });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
 const createTodo = (req, res) => {
   const { text, fecha, done } = req.body;
-  if (!text || !fecha || !done) {
-    return res
-      .status(400)
-      .json({ error: "Se requieren los campos text, fecha y done." });
+  try {
+    if (!text || !fecha || !done) {
+      return res
+        .status(400)
+        .json({ error: "Se requieren los campos text, fecha y done." });
+    }
+    const lastId = todoList.length > 0 ? todoList[todoList.length - 1].id : 0;
+    const newTodo = {
+      id: lastId + 1,
+      ...req.body,
+    };
+    todoList.push(newTodo);
+    res.status(201).json({ Task: newTodo });
+  } catch (error) {
+    console.log(error);
   }
-  const lastId = todoList.length > 0 ? todoList[todoList.length - 1].id : 0;
-  const newTodo = {
-    id: lastId + 1,
-    ...req.body,
-  };
-  todoList.push(newTodo);
-  res.status(201).json({ Task: newTodo });
 };
 
 const updateTodo = (req, res) => {
   const { text, fecha, done } = req.body;
-  if (!text || !fecha || !done) {
-    return res
-      .status(400)
-      .json({ error: "Se requieren los campos text, fecha y done." });
-  }
-  const todoId = parseInt(req.params.id);
-  const todoIndex = todoList.findIndex((todo) => todo.id === todoId);
-  if (todoIndex !== -1) {
-    todoList[todoIndex] = {
-      ...todoList[todoIndex],
-      ...req.body,
-    };
-    res.status(201).json(todoList[todoIndex]);
-  } else {
-    res.status(404).json({ error: "Todo not found" });
+  try {
+    if (!text || !fecha || !done) {
+      return res
+        .status(400)
+        .json({ error: "Se requieren los campos text, fecha y done." });
+    }
+    const todoId = parseInt(req.params.id);
+    const todoIndex = todoList.findIndex((todo) => todo.id === todoId);
+    if (todoIndex !== -1) {
+      todoList[todoIndex] = {
+        ...todoList[todoIndex],
+        ...req.body,
+      };
+      res.status(201).json(todoList[todoIndex]);
+    } else {
+      res.status(404).json({ error: "Todo not found" });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
 const deleteTodo = (req, res) => {
   const todoId = parseInt(req.params.id);
   const todoIndex = todoList.findIndex((todo) => todo.id === todoId);
-  if (todoIndex !== -1) {
-    todoList.splice(todoIndex, 1);
-    res.status(204).send();
-  } else res.status(404).json({ error: "Todo not found" });
+  try {
+    if (todoIndex !== -1) {
+      todoList.splice(todoIndex, 1);
+      res.status(204).send();
+    } else res.status(404).json({ error: "Todo not found" });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,58 +1,82 @@
 let { userList } = require("../data/userList");
 
 const getAllUsers = (req, res) => {
-  res.status(200).json({ Users: userList });
+  try {
+    res.status(200).json({ Users: userList });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const getUserById = (req, res) => {
   const userId = parseInt(req.params.id);
   const userFound = userList.find((user) => user.id === userId);
-  if (userFound) {
-    res.status(200).json(userFound);
-  } else {
-    res.status(404).json({ error: "User not found" });
+  try {
+    if (userFound) {
+      res.status(200).json(userFound);
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
 const createUser = (req, res) => {
   const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    return res.status(400).json({ error: 'Se requieren los campos name, email y password.' });
+  try {
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ error: "Se requieren los campos name, email y password." });
+    }
+    const lastId = userList.length > 0 ? userList[userList.length - 1].id : 0;
+    const newUser = {
+      id: lastId + 1,
+      ...req.body,
+    };
+    userList.push(newUser);
+    res.status(201).json({ User: newUser });
+  } catch (error) {
+    console.log(error);
   }
-  const lastId = userList.length > 0 ? userList[userList.length - 1].id : 0;
-  const newUser = {
-    id: lastId + 1,
-    ...req.body,
-  };
-  userList.push(newUser);
-  res.status(201).json({ User: newUser });
 };
 
 const updateUser = (req, res) => {
   const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    return res.status(400).json({ error: 'Se requieren los campos name, email y password.' });
-  }
-  const userId = parseInt(req.params.id);
-  const userIndex = userList.findIndex((user) => user.id === userId);
-  if (userIndex !== -1) {
-    userList[userIndex] = {
-      ...userList[userIndex],
-      ...req.body,
-    };
-    res.status(201).json(userList[userIndex]);
-  } else {
-    res.status(404).json({ error: "User not found" });
+  try {
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ error: "Se requieren los campos name, email y password." });
+    }
+    const userId = parseInt(req.params.id);
+    const userIndex = userList.findIndex((user) => user.id === userId);
+    if (userIndex !== -1) {
+      userList[userIndex] = {
+        ...userList[userIndex],
+        ...req.body,
+      };
+      res.status(201).json(userList[userIndex]);
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
 const deleteUser = (req, res) => {
   const userId = parseInt(req.params.id);
   const userIndex = userList.findIndex((user) => user.id === userId);
-  if (userIndex !== -1) {
-    userList.splice(userIndex, 1);
-    res.status(204).send();
-  } else res.status(404).json({ error: "User not found" });
+  try {
+    if (userIndex !== -1) {
+      userList.splice(userIndex, 1);
+      res.status(204).send();
+    } else res.status(404).json({ error: "User not found" });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
